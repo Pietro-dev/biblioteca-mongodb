eef31d64a4a200b38d7e5c3e75b1f2ddbcc9d3b2
Pietro Martins <pietro.pietromartins@gmail.com>
2024-06-05 13:28:20 -0300

    conexão alterada para o atlas mongodb

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function main(){
     try {
-        await mongoose.connect('mongodb://localhost/biblioteca')
+        await mongoose.connect('mongodb+srv://pietromartins:biblioteca1234@clusterbiblioteca.pd797qq.mongodb.net/biblioteca?retryWrites=true&w=majority&appName=clusterBiblioteca')
         console.log('Conectado ao banco!')
     } catch (error) {
         console.log(`Erro: ${error}`)
